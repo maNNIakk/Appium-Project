5bc2ec662e5695a09d98653e85d6558b3a1f6fb5
Renato Santos <renato.santos@reserve.com.br>
2024-01-25 02:27:18 -0300

    secao3 - finalizando secao e incluindo ultimo teste de formulário

--- src/main/java/br/com/factory/DriverFactory.java
+++ src/main/java/br/com/factory/DriverFactory.java
@@ -20,8 +20,11 @@ public class DriverFactory {
         ServerFactory.startAppiumServer();
 
         var options = new BaseOptions()
+                .amend("appium:deviceMetrics.width", 1080)
+                .amend("appium:deviceMetrics.height", 1920)
+                .amend("appium:deviceMetrics.pixelRatio", 3.0)
                 .amend("appium:udid", "RQ8M90M4B4X")
-                .amend("appium:app", "src//main//resources//CTAppium_1_2.apk")
+                .amend("appium:app", "src//main//resources//CTAppium_2_0.apk")
                 .amend("platformName", "Android")
                 .amend("appium:deviceName", "emulator-5554")
                 .amend("appium:automationName", "UiAutomator2")
@@ -38,7 +41,9 @@ public class DriverFactory {
 
         driver = new AndroidDriver(new URL("http://localhost:4723/"), options);
         driver.unlockDevice();
-        driver.findElement(AppiumBy.id("android:id/button1")).click();
+        //       Apenas para a versão 1.2
+//        driver.findElement(AppiumBy.id("android:id/button1")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Formulário\"]")).click();
     }
 
 
--- src/main/java/br/com/tests/formulario/Formulario.java
+++ src/main/java/br/com/tests/formulario/Formulario.java
@@ -3,43 +3,35 @@ package br.com.tests.formulario;
 import br.com.factory.DriverFactory;
 import io.appium.java_client.AppiumBy;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
 
-import java.util.List;
-
 public class Formulario extends DriverFactory {
-
-    @Before
-    public void setup(){
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"SeuBarriga Nativo\"]")).click();
-    }
 
 
     @Test
     public void preencherCampoNomeFormulario() {
         driver.findElement(AppiumBy.accessibilityId("nome")).sendKeys("Xablau");
-        Assert.assertEquals(driver.findElement(AppiumBy.accessibilityId("nome")).getText(),"Xablau");
-        driver.navigate().back();
+        Assert.assertEquals(driver.findElement(AppiumBy.accessibilityId("nome")).getText(), "Xablau");
+
     }
 
     @Test
     public void clicaValorCombo() {
-        driver.findElement(AppiumBy.accessibilityId("slid")).click();
-        List<WebElement> listaCombo = driver.findElements(AppiumBy.className("android.widget.CheckedTextView"));
-        for (WebElement itemCombo : listaCombo) {
-            if (itemCombo.getText().contains("XBox")) {
-                itemCombo.click();
-            }
-        }
-        Assert.assertEquals(driver.findElement(AppiumBy.id("android:id/text1")).getText(),"XBox One");
+        driver.findElement(AppiumBy.id("android:id/text1")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.CheckedTextView[@text='Nintendo Switch']")).click();
+
+        Assert.assertEquals(driver.findElement(AppiumBy.id("android:id/text1")).getText(), "Nintendo Switch");
     }
 
     @Test
-    @Ignore
-    public void marcarSwitchECheckbox(){
+    public void marcarSwitchECheckbox() {
 
+        WebElement checkBox = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"01/01/2000\"]"));
+        WebElement switc = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"06:00\"]"));
+        checkBox.click();
+        switc.click();
+
+        Assert.assertTrue(checkBox.isSelected());
     }
 }
